c7f5472bbc7558259d83aba871f26f578f9ca3bd
Ted Moore <ted@tedmooremusic.com>
2026-02-24 13:10:09 -0500

    more data types

--- examples/tests/TestToPython.py
+++ examples/tests/TestToPython.py
@@ -6,9 +6,9 @@
 from mmm_python import *
 m = MMMAudio(128, graph_name="TestToPython", package_name="examples.tests")
 m.register_callback("pitch", lambda args: print(f"pitch: {args}"))
-# for i in range(50):
-#     m.register_callback(f"val_{i}", lambda args, idx=i: print(f"val_{idx}: {args}"))
 m.register_callback("vals", lambda args: print(f"vals: {args}"))
+m.register_callback("bool", lambda args: print(f"bool: {args}"))
+m.register_callback("trig", lambda args: print(f"trig: {args}"))
 m.start_audio()
 
 m.stop_audio()
