b815645f4fdf1c0e3256ce6cf8be233faef99db3
Ted Moore <ted@tedmooremusic.com>
2026-02-23 20:14:55 -0500

    50 more only costs 1.4% cpu

--- examples/tests/TestToPython.py
+++ examples/tests/TestToPython.py
@@ -6,8 +6,8 @@
 from mmm_python import *
 m = MMMAudio(128, graph_name="TestToPython", package_name="examples.tests")
 m.register_callback("pitch", lambda args: print(f"pitch: {args}"))
-# for i in range(200):
-#     m.register_callback(f"val_{i}", lambda args, i=i: print(f"val_{i}: {args}"))
+for i in range(50):
+    m.register_callback(f"val_{i}", lambda args, idx=i: print(f"val_{idx}: {args}"))
 m.start_audio()
 
 m.stop_audio()
